d3343fd4e1b4fb4740519206fccddd9b315073de
baraboy <rizky.dwiixg36@gmail.com>
2024-03-25 15:02:08 +0800

    update time zone at maps

--- resources/views/maps/maps_tma.blade.php
+++ resources/views/maps/maps_tma.blade.php
@@ -134,7 +134,7 @@
   <script>
 
     const map = L.map('map').setView([-8.7,117.5], 9);
-
+    
     const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
